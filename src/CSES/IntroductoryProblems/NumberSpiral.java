b936f81046659d8fdbd840edf5e8de25622a5fbe
Otito Amuga <urphasin@gmail.com>
2025-07-26 18:34:31 -0600

    number spiral input test cases

--- src/CSES/IntroductoryProblems/NumberSpiral.java
+++ src/CSES/IntroductoryProblems/NumberSpiral.java
@@ -1,10 +1,31 @@
 package CSES.IntroductoryProblems;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class NumberSpiral {
     public static int findNumberInInfiniteSpiral(int y, int x) {
-        return 0;
+        int n = Math.max(y, x);
+        int start = (int)(Math.pow(n-1, 2) + 1);
+        int res = 0;
+        if (n % 2 == 0) {
+            if (y < x)
+            {
+                res = start + (y - 1);
+            }
+            else if (y > x) {
+                res = start + (n - 1) + (n - x);
+            }
+            else {
+                res = start + (n - 1);
+            }
+        }
+        else {
+            if (y < x) {
+
+            }
+        }
+        return res;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
